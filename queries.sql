491a939ce93396fb60545b5a9ed5667b87f1d86a
tmazko <tmazko@kse.org.ua>
2025-10-08 14:36:12 +0300

    adding comments in queries.sql

--- queries.sql
+++ queries.sql
@@ -1,20 +1,18 @@
 use kse;
 
+-- total summary:
+-- finds the program with more than one student that has the lowest average student grade.
+-- AND (union)
+-- finds the program and professor associated with the highest number of failed courses (grades < 60)
 
-
-
-
-
-
-
--- calculate each student's average grade
 with  student_avg as(
+	-- calculate each student's average grade
 	select s.id, s.name, s.program_id, avg(e.grade) as student_avg 
     from students s 
     right join enrollments e on s.id=e.student_id 
     group by s.id )
  (   
- -- program statistics: average grade per program
+ -- program statistics: average grade per program and limit 1: find the program with the lowest ang gra
 select 
     p.name as program_name, 
     count(st_avg.id) as students_count, 
@@ -26,10 +24,12 @@ left join student_avg as st_avg on p.id=st_avg.program_id
 group by p.code, p.name 
 having count(st_avg.id)>1 
 order by AVG(st_avg.student_avg)
+limit 1
 )
 UNION ALL
--- -- For each program and professor, count how many failed courses students had (ordered by highest fail count)
 (
+-- -- For each program finds a professor with count how many failed courses students had. 
+-- and limit 1: find the program and proffessor with the biggest num of failies
 select p.name, null as students_count, null as avg_program_grade, pr.name, count(bad_st.id) as faild_course_count 
 from courses c 
 left join professors pr on c.professor_id=pr.id 
@@ -43,16 +43,16 @@ right join(
 left join programs p on p.id=bad_st.program_id 
 group by p.code, p.name, pr.name 
 order by count(bad_st.id) desc 
-)
-limit 10;
+limit 1
+);
 
 
 
 
-
-
--- how it looked before i tried to use union and combine these two queries... 
+-- how it had looked before i tried to use union and combine these two queries... 
 -- as you can see it is quite difficalt to union it but i tried....
+
+-- finds the program (with more than one student) that has the lowest average student grade
 with student_avg as( 
 	select s.id, s.name, s.program_id, avg(e.grade) as student_avg 
 	from students s 
@@ -65,6 +65,7 @@ group by p.code, p.name having count(st_avg.id)>1
 order by AVG(st_avg.student_avg) 
 limit 1 ; 
 
+-- finds the professor with the most failed enrollments.
 select p.id, p.name, count(bad_st.id) as faild_course_count 
 from professors p 
 left join courses c on c.professor_id=p.id 
